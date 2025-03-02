64d73a31f0ea4172a2dfb6cba59f61f8e8e37c35
yongcheng.chee <yongcheng.chee@zurich.com.my>
2025-03-03 00:13:03 +0800

    Load balancer

--- main.tf
+++ main.tf
@@ -47,6 +47,38 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  load_balancer_type = "application"
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = {
+    blog-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  listeners = {
+    http_tcp_listeners = {
+      port     = 80
+      protocol = "HTTP"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source              = "terraform-aws-modules/security-group/aws"
   version             = "5.3.0"
